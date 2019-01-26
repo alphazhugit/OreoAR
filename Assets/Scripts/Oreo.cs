d1bee9523665f9c1a25238ace865c84b485e1674
alpha <alphazhu@hotmail.com>
2019-01-26 14:22:00 +0800

    emmm...有点样子了

--- Assets/Scripts/Oreo.cs
+++ Assets/Scripts/Oreo.cs
@@ -1,55 +1,73 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
- 
 
 public class Oreo : MonoBehaviour {
 
     public GameObject m_Ao;
     public GameObject m_Ao_up;
     public GameObject m_Li;
-    public Transform oreoP;
+    private GameObject oreoP;
 
+    public Canvas settingCanvas;
+    public Canvas backCanvas;
 
     private List<GameObject> Aoliao = new List<GameObject>();
-    // Use this for initialization
-    void Start ()
+    VoidArControl arcontrol = new VoidArControl();
+
+    void Start()
     {
-        Aoliao.Add(m_Ao);
-        Aoliao.Add(m_Li);
-        Aoliao.Add(m_Li);
-        Aoliao.Add(m_Ao);
-        Aoliao.Add(m_Ao);
-        Aoliao.Add(m_Ao);
-        Aoliao.Add(m_Li);
-        Aoliao.Add(m_Ao);
-        Apply();
+        Init();
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
-    void Apply()
+    void Init()
     {
-        Quaternion qua = Quaternion.identity;
-
-        for (int i = 0; i < Aoliao.Count; ++ i)
+        oreoP = new GameObject("oreoP");
+        oreoP.transform.parent = GameObject.Find("Oreo").transform;
+        backCanvas.enabled = false;
+        settingCanvas.enabled = true;
+        if (Aoliao.Count > 0)
         {
-            if (i == Aoliao.Count - 1)
-            {
-                Aoliao[i] = m_Ao_up;
-            }
-            Instantiate(Aoliao[i], new Vector3(0,i * 0.1f,0), qua, oreoP);
+            Aoliao.Clear();
         }
     }
-    private void OnGUI()
+
+    public void Apply()
     {
-        //if (GUILayout.Button("Apply"))
-        //{
-        //    Apply();
-        //}
+        if (Aoliao.Count > 0)
+        {
+            Quaternion qua = Quaternion.identity;
+
+            for (int i = 0; i < Aoliao.Count; ++i)
+            {
+                if (i == Aoliao.Count - 1 && Aoliao[i] == m_Ao)
+                {
+                    Aoliao[i] = m_Ao_up;
+                }
+                Instantiate(Aoliao[i], new Vector3(0, i * 0.1f, 0), qua, oreoP.transform);
+            }
+        }
+
+        settingCanvas.enabled = false;
+        backCanvas.enabled = true;
+
+        arcontrol.startMarkerlessTracking();
+    }
+
+    public void Add_Ao()
+    {
+        Aoliao.Add(m_Ao);
+    }
+
+    public void Add_Li()
+    {
+        Aoliao.Add(m_Li);
+    }
+    public void Reset()
+    {
+        if (Aoliao.Count > 0)
+        {
+            Destroy(oreoP);
+            Init();
+        }
     }
 }
--- Assets/VoidARDemo/Scripts/Demo.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-public class Demo : MonoBehaviour {
-    public void OnMenuSelect(string sceneName) {
-        Application.LoadLevel(sceneName);
-    }
-}
